a3fddaaad4d59c81ae6197302ff3d4c70f202a2f
vunikabryson <vunikabryson@gmail.com>
2026-06-12 00:30:54 +0200

    Update tailwind.config.js.ts

--- tailwind.config.js.ts
+++ tailwind.config.js.ts
@@ -1,6 +1,4 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -41,7 +39,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
-}
-
-export default config
+  plugins: [],
+};
